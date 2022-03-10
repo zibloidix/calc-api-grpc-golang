e562a10908b92c9139a71f761b7497428b07ba13
Aleksey Lomov <zibloidix@gmail.com>
2022-03-11 10:27:43 +1100

    DEV-120 1. Реализован клиент

--- client.go
+++ client.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/zibloidix/calc-api-grpc-golang/calc_api_pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Client start")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+	c := calc_api_pb.NewCalcServiceClient(cc)
+
+	reqAdd := &calc_api_pb.AddRequest{
+		A: 5,
+		B: 5,
+	}
+
+	reqSubtract := &calc_api_pb.SubtractRequest{
+		A: 500,
+		B: 100,
+	}
+
+	reqMultiply := &calc_api_pb.MultiplyRequest{
+		A: 5,
+		B: 5,
+	}
+
+	reqDivide := &calc_api_pb.DivideRequest{
+		A: 25,
+		B: 5,
+	}
+
+	resAdd, err := c.Add(context.Background(), reqAdd)
+	resSubtract, err := c.Subtract(context.Background(), reqSubtract)
+	resMultiply, err := c.Multiply(context.Background(), reqMultiply)
+	resDivide, err := c.Divide(context.Background(), reqDivide)
+
+	log.Printf("Add: %v", resAdd)
+	log.Printf("Subtract: %v", resSubtract)
+	log.Printf("Multiply: %v", resMultiply)
+	log.Printf("Divide: %v", resDivide)
+}
